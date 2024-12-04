7efc457d9cfbf9c9339ff551934ebc62286bd941
silverspade-gh <silver@bucketofdata.com>
2024-12-04 15:10:14 -0500

    Update pyconn_db.py

--- pyconn_db.py
+++ pyconn_db.py
@@ -1,14 +1,8 @@
 # If not installed, use pip install psycopg2-binary
 import psycopg2
 
-# Database connection parameters
-db_params = {
-    'dbname': 'aditi',
-    'user': 'postgres',
-    'password': 'notmyactualpassword', # replace notmyactualpassword with your actual password
-    'host': 'db.notmyactuallocation.us-east-1.rds.amazonaws.com',  # replace db.notmyactuallocation with the db's endpoint
-    'port': '5432'        # Default PostgreSQL port
-}
+# Database Connection Credentials
+from db_creds.py import db_params
 
 try:
     # Establish the connection
