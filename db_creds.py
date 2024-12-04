ef5f274f8773693f2131926acbefa15d5a5e645e
silverspade-gh <silver@bucketofdata.com>
2024-12-04 15:09:38 -0500

    Create db_creds.py
    Module db_creds containing database connection parameters

---
+++ db_creds.py
@@ -0,0 +1,10 @@
+# db_creds.py
+# Database connection parameters
+
+db_params = {
+    'dbname': 'aditi',
+    'user': 'postgres',
+    'password': 'notmyactualpassword', # replace notmyactualpassword with your actual password
+    'host': 'db.notmyactuallocation.us-east-1.rds.amazonaws.com',  # replace db.notmyactuallocation with the db's endpoint
+    'port': '5432'        # Default PostgreSQL port
+}
